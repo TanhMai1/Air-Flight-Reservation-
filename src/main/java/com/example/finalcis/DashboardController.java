cea9d112bd2a25e31f4e86db60f6fc91241ae1dd
Tan Mai <tanmai1126.com>
2023-12-05 23:28:48 -0500

    Login and Register Button

---
+++ src/main/java/com/example/finalcis/DashboardController.java
@@ -0,0 +1,12 @@
+package com.example.finalcis;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class DashboardController {
+
+    @FXML
+    private Label welcomeLabel;
+
+    // Additional methods to handle dashboard actions
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.finalcis {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.finalcis to javafx.fxml;
