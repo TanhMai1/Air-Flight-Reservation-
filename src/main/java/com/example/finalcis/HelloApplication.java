ea1f75adad5206d25a6541c9a780d6428989cb75
Tan Mai <tanmai1126.com>
2023-12-06 19:55:03 -0500

    Deleted test class, resize application, added Title screen

--- src/main/java/com/example/finalcis/HelloApplication.java
+++ src/main/java/com/example/finalcis/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainScreen.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Login Screen!");
         stage.setScene(scene);
         stage.show();
