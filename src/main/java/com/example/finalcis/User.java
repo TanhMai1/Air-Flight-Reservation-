2f9e1f894e212be4d2ae2af8dd16d1b6d2a83244
Tan Mai <tanmai1126.com>
2023-12-25 21:58:25 -0500

    Not used getters and setters

--- src/main/java/com/example/finalcis/User.java
+++ src/main/java/com/example/finalcis/User.java
@@ -39,23 +39,14 @@ public class User {
         return firstName;
     }
 
-    public void setFirstName(String FirstName) {
-        this.firstName = firstName;
-    }
 
     public String getLastName() {return lastName;}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
 
     public String getZipcode() {
         return zipcode;
@@ -69,41 +60,26 @@ public class User {
         return state;
     }
 
-    public void setState(String state) {
-        this.state = state;
-    }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     public String getSsn() {
         return ssn;
     }
 
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
 
     public String getSecurityAnswer() {
         return securityAnswer;
